8f00f1a8769992a61b24a85aea64744ee3ee2843
Eamon <eamonmohare@gmail.com>
2021-03-24 00:34:37 -0400

    added lines to print to file 5 times

--- testSHT20.py
+++ testSHT20.py
@@ -1,8 +1,18 @@
 from sensor import SHT20
 
 # I2C bus=1, Address=0x40
+#write the data into data.txt
+file1 = open('data.txt', 'w')
 sht = SHT20(1, 0x40)
-h, t = sht.all()  # read both at once
+for x in range(5)
+    h, t = sht.all()  # read both at once
+    file1.writelines(h)
+    file1.write("\n")
+    file1.writelines(l)
+    file1.write("\n")
+
+
+file1.close()
 
 #h = sht.humidity()  # read humidity
 print(h)            # namedtuple
